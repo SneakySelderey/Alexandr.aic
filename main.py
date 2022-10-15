7e4be30863f226c32ea23915b2b8bf005a4ba0a2
Selderey <sneakyselderey@yandex.ru>
2022-10-15 16:32:50 +0300

    help command refactored

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 # import logging
 from decouple import config
 from data import db_session
+from help_command import SupremeHelpCommand
 
 
 intents = discord.Intents.default()
@@ -12,6 +13,7 @@ intents.message_content = True
 dashes = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
 
 bot = commands.Bot(command_prefix='$', intents=intents, activity=discord.Activity(type=discord.ActivityType.listening, name="SCP Foundation databases"))
+bot.help_command = SupremeHelpCommand()
 TOKEN = config('DISCORD_TOKEN', default='not found')
 
 
